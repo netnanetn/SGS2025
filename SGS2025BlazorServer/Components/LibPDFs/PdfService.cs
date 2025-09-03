f766a19effe0744cfd51ad8d4e1bf36d7792006e
trungnc <trungnc.bk@gmail.com>
2025-09-03 08:55:27 +0700

    up

--- SGS2025BlazorServer/Components/LibPDFs/PdfService.cs
+++ SGS2025BlazorServer/Components/LibPDFs/PdfService.cs
@@ -9,106 +9,191 @@ namespace SGS2025BlazorServer.Components.LibPDFs
         public async Task<byte[]> GeneratePdfAsync(TblScale scale, string pageSize = "A4")
         {
             var htmlContent = $@"
-            <!DOCTYPE html>
-            <html lang='vi'>
-            <head>
-                <meta charset='UTF-8'>
-                <title>In thông tin xe</title>
-                <link href='https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap' rel='stylesheet'>
-                <style>
-                    @page {{ size: {pageSize}; margin: 20mm; }}
-                    body {{ 
-                        font-family: 'Roboto', Arial, sans-serif; 
-                        font-size: 12pt; 
-                        color: #333; 
-                    }}
-                    .container {{ 
-                       
-                        max-width: 100%; 
-                        margin: 0 auto; 
-                        padding: 20px; 
-                        border: 2px solid #007bff; 
-                        border-radius: 10px; 
-                        background-color: #f9f9f9; 
-                    }}
-                    h1 {{ 
-                        text-align: center; 
-                        color: #007bff; 
-                        font-weight: 700; 
-                    }}
-                    table {{ 
-                        width: 100%; 
-                        border-collapse: collapse; 
-                        margin-top: 20px; 
-                        box-shadow: 0 2px 5px rgba(0,0,0,0.1); 
-                        table-layout: fixed;
-                        border-collapse: collapse;
-                    }}
-                    th, td {{ 
-                        border: 1px solid #ddd; 
-                        padding: 10px; 
-                        text-align: left; 
-                        word-wrap: break-word;
-                    }}
-                    th {{ 
-                        background-color: #007bff; 
-                        color: white; 
-                        font-weight: 700; 
-                    }}
-                    tr:nth-child(even) {{ 
-                        background-color: #f2f2f2; 
-                    }}
-                    .highlight {{ 
-        font-weight: bold; 
-        color: #e91e63; 
-    }} 
-    .header_1 {{
-        font-size: 18px;
-        font-weight: 700;
-        margin-bottom: 5px;
-    }}
-    .header_2 {{
-        font-size: 16px;
-        font-weight: 700;
-    }}
-    .stt {{
-        text-align: center;
-    }}
-    .row-item-container {{
-        display: flex;
-    }}
-    .image-grid {{
-        display: grid;
-        grid-template-columns: repeat(3, 1fr);
-        gap: 10px;
-        margin-top: 20px;
-    }}
-    .image-grid img {{
-        width: 100%;
-        height: auto;
-        border: 1px solid #ddd;
-        border-radius: 5px;
-    }}
-    .two-columns {{
-        display: flex;
-        gap: 20px;
-        margin-top: 20px;
-    }}
-    .column {{
-        flex: 1;
-    }}
-    .row-item-container {{
-        display: flex;
-        justify-content: flex-start;
-        gap: 10px;
-        margin-bottom: 8px;
-    }}
-    .row-item-container div:first-child {{
-        font-weight: bold;
-    }}
-                </style>
-            </head>
-            <body>
+<!DOCTYPE html>
+<html lang=""vi"">
+<head>
+    <meta charset=""UTF-8"">
+    <title>Xem thử phiếu in</title>
+    <link href=""https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"" rel=""stylesheet"">
+    <style>
+        @page {{
+            size: A4;
+            margin: 10mm;
+        }}
+
+        body {{
+            font-family: 'Roboto', Arial, sans-serif;
+            font-size: 12pt;
+            color: #333;
+        }}
+
+        .container {{
+            width: 170mm;
+            margin: 0 auto;
+            padding: 10mm;
+        }}
+
+        h1 {{
+            text-align: center;
+            font-weight: 800;
+            font-size: 28px;
+        }}
+
+        table {{
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 10mm;
+            table-layout: fixed;
+        }}
+
+        th, td {{
+            border: 1px solid #ddd;
+            padding: 5mm;
+            text-align: left;
+            word-wrap: break-word;
+        }}
+
+        th {{
+            background-color: #007bff;
+            color: white;
+            font-weight: 700;
+        }}
+
+        tr:nth-child(even) {{
+            background-color: #f2f2f2;
+        }}
+
+        .highlight {{
+            font-weight: 700;
+            color: black;
+            font-size: 16px;
+        }}
+        .header_1 {{
+            font-size: 18px;
+            font-weight: 700;
+            margin-bottom: 5px;
+        }}
+        .header_2 {{
+            font-size: 16px;
+            font-weight: 700;
+        }}
+        .stt{{
+            text-align: center;
+            font-weight: 700;
+        }}
+        .row-item-container{{
+            display: flex;
+        }}
+        .image-grid {{
+            display: grid;
+            grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+            gap: 10px;
+            margin-top: 20px;
+        }}
+
+            .image-grid img {{
+                width: 100%;
+                height: auto;
+                border: 1px solid #ddd;
+                border-radius: 5px;
+            }}
+        .two-columns {{
+            display: flex;
+            gap: 20px;
+            margin-top: 20px;
+        }}
+
+        .column {{
+            flex: 1;
+        }}
+
+        .row-item-container {{
+            display: flex;
+            justify-content: flex-start;
+            gap: 10px;
+            margin-bottom: 8px;
+        }}
+
+            .row-item-container div:first-child {{
+                font-weight: bold;
+                width: 120px;
+            }}
+        .info-table {{
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 10px;
+        }}
+
+            .info-table th,
+            .info-table td {{
+                padding: 6px 10px;
+                border: 1px solid #ddd;
+                text-align: left;
+                vertical-align: top;
+            }}
+
+            .info-table th {{
+                width: 120px;
+                background-color: #f2f2f2;
+                font-weight: bold;
+                color: black;
+            }}
+
+        .signature-1{{
+            font-weight: 700;
+        }}
+
+        .signature-section {{
+            display: flex;
+            justify-content: space-between;
+            margin-top: 50px;
+            text-align: center;
+        }}
+
+        .signature-col {{
+            flex: 1;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            height: 150px;
+        }}
+
+            .signature-col div:first-child {{
+                font-weight: bold;
+            }}
+
+        .signature-placeholder {{
+            font-style: italic;
+            font-size: 12px;
+        }}
+        .scale-table {{
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 10px;
+        }}
+
+            .scale-table th,
+            .scale-table td {{
+                border: 1px solid #ddd;
+                padding: 8px;
+                text-align: center;
+            }}
+
+                .scale-table th.short {{
+                    width: 15%;
+                }}
+
+                .scale-table th.long {{
+                    width: 26%;
+                }}
+
+            .scale-table th {{
+                background-color: #007bff;
+                color: black;
+            }}
+    </style>
+</head>
+<body>
     <div class=""container"">
         <div class=""header"">
             <div class=""header_1"">CÔNG TY TNHH PRIMEWOOD CORPORATION (VIỆT NAM)</div>
@@ -116,50 +201,52 @@ namespace SGS2025BlazorServer.Components.LibPDFs
         </div>
         <h1>PHIẾU CÂN XE</h1>
         <div class=""stt"">STT: NH-39</div>
-        <div class=""two-columns"">
-            <!-- Cột trái -->
-            <div class=""column"">
-                <div class=""row-item-container"">
-                    <div>- Hàng hóa :</div> <div>Dăm gỗ</div>
-                </div>
-                <div class=""row-item-container"">
-                    <div>- Khách hàng :</div> <div>Công Ty TNHH Chế Biến Lâm Sản An Phước</div>
-                </div>
-                <div class=""row-item-container"">
-                    <div>- Địa chỉ :</div> <div>Vũng Áng, Kỳ Lợi, Kỳ Anh</div>
-                </div>
-                <div class=""row-item-container"">
-                    <div>- Biển số xe :</div> <div>37H55055</div>
-                </div>
-                <div class=""row-item-container"">
-                    <div>- Ghi chú :</div> <div>Vũng Áng, Kỳ Lợi, Kỳ Anh</div>
-                </div>
-            </div>
-
-            <!-- Cột phải -->
-            <div class=""column"">
-                <div class=""row-item-container"">
-                    <div>- Số điện thoại :</div> <div>0912 345 678</div>
-                </div>
-                <div class=""row-item-container"">
-                    <div>- Kiểu cân :</div> <div>Cân nhập hàng</div>
-                </div>
-            </div>
+        <div class="""">
+            <table class=""info-table"">
+                <tr>
+                    <th>Hàng hóa</th>
+                    <td>Dăm gỗ</td>
+                </tr>
+                <tr>
+                    <th>Khách hàng</th>
+                    <td>Công Ty TNHH Chế Biến Lâm Sản An Phước</td>
+                </tr>
+                <tr>
+                    <th>Địa chỉ</th>
+                    <td>Vũng Áng, Kỳ Lợi, Kỳ Anh</td>
+                </tr>
+                <tr>
+                    <th>Biển số xe</th>
+                    <td>37H55055</td>
+                </tr>
+                <tr>
+                    <th>Ghi chú</th>
+                    <td>Vũng Áng, Kỳ Lợi, Kỳ Anh</td>
+                </tr>
+                <tr>
+                    <th>Số điện thoại</th>
+                    <td>0912 345 678</td>
+                </tr>
+                <tr>
+                    <th>Kiểu cân</th>
+                    <td>Cân nhập hàng</td>
+                </tr>
+            </table>
         </div>
-        <table>
+        <table class=""scale-table"">
             <thead>
                 <tr>
-                    <th>TL xe vào</th>
-                    <th>TL xe ra</th>
-                    <th>TL hàng</th>
-                    <th>Ngày giờ cân vào</th>
-                    <th>Ngày giờ cân ra</th>
+                    <th class=""short"">TL xe vào</th>
+                    <th class=""short"">TL xe ra</th>
+                    <th class=""short"">TL hàng</th>
+                    <th class=""long"">Ngày giờ cân vào</th>
+                    <th class=""long"">Ngày giờ cân ra</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                    <td>12,000 kg</td>
-                    <td>6,500 kg</td>
+                    <td class=""highlight"">12,000 kg</td>
+                    <td class=""highlight"">6,500 kg</td>
                     <td class=""highlight"">5,500 kg</td>
                     <td>28/08/2025 08:30</td>
                     <td>28/08/2025 10:15</td>
@@ -167,7 +254,6 @@ namespace SGS2025BlazorServer.Components.LibPDFs
             </tbody>
         </table>
         <div>
-            <div>Ảnh chụp xe</div>
             <div class=""image-grid"">
                 <img src=""https://trungvietsoftware-cdn.cdn.vccloud.vn/tramda/tvs/tvs_1756457126_113236022.jpeg"" alt=""Ảnh 1"" />
                 <img src=""https://trungvietsoftware-cdn.cdn.vccloud.vn/tramda/tvs/tvs_1756457126_113236022.jpeg"" alt=""Ảnh 2"" />
@@ -177,9 +263,24 @@ namespace SGS2025BlazorServer.Components.LibPDFs
                 <img src=""https://trungvietsoftware-cdn.cdn.vccloud.vn/tramda/tvs/tvs_1756457126_113236022.jpeg"" alt=""Ảnh 6"" />
             </div>
         </div>
+        <div class=""signature-section"">
+            <div class=""signature-col"">
+                <div class=""signature-1"">Nhân viên bàn cân</div>
+                <div class=""signature-placeholder"">(Ký và ghi rõ họ tên)</div>
+            </div>
+            <div class=""signature-col"">
+                <div class=""signature-1"">Khách hàng</div>
+                <div class=""signature-placeholder"">(Ký và ghi rõ họ tên)</div>
+            </div>
+            <div class=""signature-col"">
+                <div class=""signature-1"">Công ty TNHH Primewood</div>
+                <div class=""signature-placeholder"">(Ký và ghi rõ họ tên)</div>
+            </div>
+        </div>
     </div>
 </body>
-            </html>";
+</html>
+";
 
             var tempHtmlPath = Path.Combine(Path.GetTempPath(), "temp.html");
             await File.WriteAllTextAsync(tempHtmlPath, htmlContent, Encoding.UTF8);
--- SGS2025BlazorServer/wwwroot/js/site.js
+++ SGS2025BlazorServer/wwwroot/js/site.js
@@ -1,4 +1,4 @@
-﻿function printSilent(base64String) {
+﻿function printSilent2(base64String) {
     const iframe = document.createElement('iframe');
     iframe.style.display = 'none';
     iframe.src = 'data:application/pdf;base64,' + base64String;
@@ -14,7 +14,34 @@
         setTimeout(() => document.body.removeChild(iframe), 1000);
     };
 }
+function printSilent(base64String) {
+    const byteCharacters = atob(base64String);
+    const byteNumbers = new Array(byteCharacters.length);
+    for (let i = 0; i < byteCharacters.length; i++) {
+        byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    const byteArray = new Uint8Array(byteNumbers);
+    const blob = new Blob([byteArray], { type: "application/pdf" });
+    const blobUrl = URL.createObjectURL(blob);
 
+    const iframe = document.createElement("iframe");
+    iframe.style.display = "none";
+    iframe.src = blobUrl;
+    document.body.appendChild(iframe);
+
+    iframe.onload = function () {
+        try {
+            iframe.contentWindow.focus();
+            iframe.contentWindow.print();
+        } catch (e) {
+            console.error("Lỗi khi in:", e);
+        }
+        setTimeout(() => {
+            URL.revokeObjectURL(blobUrl); // giải phóng memory
+            document.body.removeChild(iframe);
+        }, 2000);
+    };
+}
 function saveAsFile(filename, base64String) {
     const link = document.createElement('a');
     link.href = 'data:application/pdf;base64,' + base64String;
